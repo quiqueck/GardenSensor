2675b10c837b8c59a9fc626f4505a6f4524ad2ae
Frank Bauer <developer@ambertation.de>
2019-08-29 14:59:58 +0200

    Added toggles for different graphs

--- dataview-frontend/src/pages/Graphs.vue
+++ dataview-frontend/src/pages/Graphs.vue
@@ -1,7 +1,53 @@
 <template>
-    <q-page class="flex">
-        <graph :values="displayedData"></graph>
-    </q-page>
+    <q-card class="my-card">
+        <q-card-section>
+            <graph :values="displayedData"></graph>
+        </q-card-section>
+        <q-separator></q-separator>
+        <q-card-section>
+            <q-toggle
+                v-model="showTemperature"
+                color="red"
+                icon="ion-ios-thermometer"
+                label="Temperatur"
+            ></q-toggle>
+
+            <q-toggle
+                v-model="showHumidity"
+                color="indigo-5"
+                icon="ion-ios-water"
+                label="Luftfeuchtigkeit"
+            ></q-toggle>
+
+            <q-toggle
+                v-model="showBrightness"
+                color="orange"
+                icon="ion-ios-sunny"
+                label="Helligkeit"
+            ></q-toggle>
+
+            <q-toggle
+                v-model="showBrightnessMT"
+                color="pink-14"
+                icon="ion-ios-contract"
+                label="Sensitivität Helligkeit"
+            ></q-toggle>
+
+            <q-toggle
+                v-model="showMoisture"
+                color="green"
+                icon="ion-ios-leaf"
+                label="Bodenfeuchtigkeit"
+            ></q-toggle>
+
+            <q-toggle
+                v-model="showVoltage"
+                color="blue-11"
+                icon="ion-ios-battery-charging"
+                label="Spannung"
+            ></q-toggle>
+        </q-card-section>
+    </q-card>
 </template>
 
 <style></style>
@@ -17,7 +63,13 @@ export default {
         return {
             interval: null,
             range: 3,
-            rangeUnit: "day"
+            rangeUnit: "day",
+            showTemperature: true,
+            showHumidity: true,
+            showBrightness: true,
+            showMoisture: true,
+            showVoltage: true,
+            showBrightnessMT: false
         };
     },
     components: {
@@ -26,15 +78,16 @@ export default {
     computed: {
         ...mapState("sensor", ["sensorData"]),
         displayedData() {
+            let datasets = [];
+            if (this.showTemperature) datasets.push(this.temperatures);
+            if (this.showHumidity) datasets.push(this.humidity);
+            if (this.showVoltage) datasets.push(this.voltage);
+            if (this.showMoisture) datasets.push(this.moisture);
+            if (this.showBrightness) datasets.push(this.brightness);
+            if (this.showBrightnessMT) datasets.push(this.brightnessMT);
             return {
                 id: moment().format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS),
-                data: [
-                    this.temperatures,
-                    this.humidity,
-                    this.voltage,
-                    this.moisture,
-                    this.brightness
-                ]
+                data: datasets
             };
         },
         values() {
@@ -107,9 +160,12 @@ export default {
         },
         createFilteredMappedSet(label, clas, unit, filter, mapping) {
             const refTime = moment().subtract(this.range, this.rangeUnit);
+            const regex = /[^-\s]+-(\S+)\b/;
+            const match = regex.exec(clas);
             return {
                 label: label,
                 class: clas,
+                color: match && match.length > 0 ? match[1] : "gray",
                 unit: unit,
                 values: this.values
                     .filter(i => {
